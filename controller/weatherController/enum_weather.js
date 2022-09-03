947700b300500d238efbba847706d208d39153b4
Vinicius SIlvestre <vine.ort27@gmail.com>
2022-09-03 13:07:00 -0300

    Folder structure change

--- controller/weatherController/enum_weather.js
+++
@@ -1,8 +0,0 @@
-export var enumWeater;
-(function (enumWeater) {
-    enumWeater[enumWeater["rain"] = 0] = "rain";
-    enumWeater[enumWeater["sun"] = 1] = "sun";
-    enumWeater[enumWeater["snow"] = 2] = "snow";
-    enumWeater[enumWeater["clouds"] = 3] = "clouds";
-    enumWeater[enumWeater["wind"] = 4] = "wind";
-})(enumWeater || (enumWeater = {}));
--- main.js
+++
@@ -1,4 +0,0 @@
-import { Game } from "./model/game/game.js";
-let game = new Game();
-game.create();
-game.gameLoop();
